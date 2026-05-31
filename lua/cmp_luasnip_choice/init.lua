2530a3f3cf50867792ddf55e3138fe0bcf8e0a34
shootingstar <jinhung.c@gmail.com>
2026-05-31 14:57:11 +0800

    fix: Rewrite source.complete()

--- lua/cmp_luasnip_choice/init.lua
+++ lua/cmp_luasnip_choice/init.lua
@@ -51,24 +51,24 @@ function M.source:get_keyword_pattern()
 end
 
 function M.source:complete(_, callback)
-    local items = {}
-
     local choices_ok, choice_docstrings = pcall(require("luasnip").get_current_choices)
-    if not choices_ok then
-        -- no choice active: return no completion-items.
-        callback(items)
-    end
 
-    for _, choice_docstring in ipairs(choice_docstrings) do
-        table.insert(items, {
-            label = choice_docstring,
-            word = "",
-            index = _,
-            documentation = choice_docstring,
-            kind = cmp.lsp.CompletionItemKind.Snippet,
-        })
+    if choices_ok then
+        local items = {}
+
+        for _, choice_docstring in ipairs(choice_docstrings) do
+            table.insert(items, {
+                label = choice_docstring,
+                word = "",
+                documentation = choice_docstring,
+                kind = cmp.lsp.CompletionItemKind.Snippet,
+            })
+        end
+
+        callback(items)
+    else
+        callback {}
     end
-    callback(items)
 end
 
 function M.source:get_debug_name()
